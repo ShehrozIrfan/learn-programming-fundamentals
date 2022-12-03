c6372c1cd66bf152699a0f8edb812c7fad712c40
Shehroz Irfan <>
2022-12-03 13:23:44 +0500

    [Adding Colors] Program to add colors to CPP console added

---
+++ C++/1. Adding colors in C++/adding colors in c++.cpp
@@ -0,0 +1,44 @@
+/*
+We can add colors to our text and background in C++:
+
+0=black         
+1=blue
+2=green
+3=aqua
+4=red
+5=purple
+6=yellow
+7=white
+8=gray
+9=light blue
+A=Light Green
+B=Light Aqua
+C=Light Red
+D=Light Purple
+E=Light Yellow
+F=Bright White
+
+*/
+// Program to Add colors.
+
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	//For adding colors we just need to add a single command that is:-->
+	
+   system("color 6f");
+   // 6 is for yellow and 'f' is for bright white.
+   
+   
+   cout<<"\t\t\t I like adding colors to C++"<<"\n";
+   
+   
+
+   return 0;
+   
+   //See the magic in output.
+}
